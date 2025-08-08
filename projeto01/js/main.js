a7f00d1bbbf2945b12fee1c73d7742e9cc0a90b4
natancarff <natanael.wlan@gmail.com>
2025-08-08 16:58:11 -0300

    atualização

--- projeto01/js/main.js
+++ projeto01/js/main.js
@@ -2,8 +2,8 @@
 
 // Declare as variáveis globais APENAS AQUI.
 // Elas serão acessíveis em todas as cenas.
-let score = 0;
-let gameOver = false;
+// let score = 0;
+// let gameOver = false;
 
 const config = {
     type: Phaser.AUTO,
